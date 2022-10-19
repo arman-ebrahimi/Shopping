565e6abee29397a1e46cdf5716ad17e084db47f3
Arman Ebrahimi <ebrahimi.arman72@gmail.com>
2022-10-19 13:54:17 -0700

    fixed a bug

--- src/components/Cart/cart-icon.js
+++ src/components/Cart/cart-icon.js
@@ -4,7 +4,7 @@ export const CartIcon = () => {
     const numbersProducts = useSelector(state => state.cart).length;
     return(
         <>
-            <img width="30" src="/images/263142.png" />
+            <img width="30" src="/images/263142.png" alt="cart" />
             {numbersProducts > 0 &&
                 <span style={{position: "absolute", marginTop: "-5px", marginLeft: "-20px"}}
                       className="badge bg-danger rounded-pill">{numbersProducts}
